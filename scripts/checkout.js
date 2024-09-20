b48eb78cb14a00358dc2389cdaf41f3b9bfb5afc
Kai <kharimobley@gmail.com>
2024-09-20 06:08:22 -0400

    Updated delete link to remove items from HTML.

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -20,7 +20,8 @@ cart.forEach((cartItem) => {
 
 
  cartSummaryHTML += `
-    <div class="cart-item-container">
+    <div class="cart-item-container
+    js-cart-item-container-${matchingProduct.id}">
       <div class="delivery-date">
         Delivery date: Tuesday, June 21
       </div>
@@ -107,5 +108,9 @@ document.querySelectorAll('.js-delete-link')
     link.addEventListener('click', () => {
       const { productId } = link.dataset;
       removeFromCart(productId);
+
+      const container = document
+      .querySelector(`.js-cart-item-container-${productId}`);
+      container.remove();
     });
   });
