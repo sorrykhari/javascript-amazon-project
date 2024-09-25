c17ede3cc98cdfc8607ffa3c21e6417b6620deef
Khari Mobley <7447993@ups.com>
2024-09-24 23:45:32 -0400

    Made page summary fully interactive.

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -3,6 +3,7 @@ import { products } from "../../data/products.js";
 import formatCurrency from "../utils/money.js";
 import  dayjs from "https://unpkg.com/dayjs@1.11.10/esm/index.js";
 import { deliveryOptions, getDeliveryOption } from "../../data/deliveryOptions.js"
+import { renderPaymentSummary } from "./paymentSummary.js";
 
 export function renderOrderSummary() {
 
@@ -155,6 +156,7 @@ export function renderOrderSummary() {
         const container = document
         .querySelector(`.js-cart-item-container-${productId}`);
         container.remove();
+        renderPaymentSummary();
       });
     });
 
@@ -199,6 +201,8 @@ export function renderOrderSummary() {
 
             // Update header checkout when product quantity changed in input
             updateCheckoutQuantity();
+            renderOrderSummary();
+            renderPaymentSummary();
           });
         });
       });
@@ -210,6 +214,7 @@ export function renderOrderSummary() {
         const { productId, deliveryOptionId } = element.dataset;
         updateDeliveryOption(productId, deliveryOptionId);
         renderOrderSummary();
+        renderPaymentSummary();
       });
     }); 
 } 
