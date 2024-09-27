144e62c2a3732984e916cb8d17d8daaf038b0fc5
Kai <kharimobley@gmail.com>
2024-09-27 07:56:57 -0400

    Order Summary Test created

---
+++ tests-jasmine/checkout/orderSummaryTest.js
@@ -0,0 +1,7 @@
+import { renderOrderSummary } from "../../scripts/checkout/orderSummary.js"
+
+describe('test suite: renderOrderSummary', () => {
+    it('displays the cart', () => {
+        
+    })
+});
