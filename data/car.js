b19db74eef93d471f6a12e76fbdfdc3c3d391940
Kai <kharimobley@gmail.com>
2024-10-01 01:46:56 -0400

    Added more classes to some products

--- data/car.js
+++ data/car.js
@@ -1,23 +1,27 @@
 class Car{
-  brand;
-  model;
+  #brand;
+  #model;
   speed = 0;
   isTrunkOpen = false;
 
   constructor(carInfo) {
-    this.brand = carInfo.brand;
-    this.model = carInfo.model;
+    console.log(carInfo.brand);
+    this.#brand = carInfo.brand;
+    this.#model = carInfo.model;
   }
 
   displayInfo() {
-    console.log(`${this.brand} ${this.model},
+    console.log(`${this.#brand} ${this.#model},
        Speed: ${this.speed} km/h,
        Trunk: ${this.isTrunkOpen ? 'Open': 'Closed'}`);
   }
 
   go() {
-    if (this.speed <= 195) {
+    if ((this.speed <= 195) && !this.isTrunkOpen) {
       this.speed += 5;
+    }
+    else if (this.isTrunkOpen) {
+      console.log('Trunk is open.')
     }
   }
 
@@ -28,9 +32,16 @@ class Car{
   }
 
   openTrunk() {
-    if (this.speed != 0) {
+    if (this.speed === 0) {
       this.isTrunkOpen = true;
     }
+    else {
+      console.log('Car is moving.');
+    }
+  }
+
+  closeTrunk() {
+    this.isTrunkOpen = false;
   }
 }
 
@@ -44,5 +55,33 @@ const car2 = new Car({
   model: 'Model 3'
 });
 
-car1.displayInfo()
-car2.displayInfo();
+class Racecar extends Car {
+  acceleration;
+  constructor(raceCarInfo) {
+    super(raceCarInfo);
+    this.acceleration = raceCarInfo.acceleration;
+  }
+
+  go() {
+    if (this.isTrunkOpen) {
+      console.log('Trunk is open.');
+      return;
+    }
+    this.accelerate();
+  }
+
+  accelerate() {
+    if(this.speed < 300) {
+      this.speed += this.acceleration;
+      if(this.speed > 300) {
+        this.speed = 300;
+      }
+    }
+  }
+}
+
+const car3 = new Racecar({
+  brand: 'McLaren',
+  model: 'F1',
+  acceleration: 20
+});
--- data/products.js
+++ data/products.js
@@ -69,6 +69,30 @@ class Clothing extends Product {
   }
 }
 
+class Appliance extends Product {
+  type;
+  instructionsLink;
+  warrantyLink;
+
+  constructor(productDetails) {
+    super(productDetails); // Calls constructor of parent class
+    this.type = productDetails.type;
+    this.instructionsLink = productDetails.instructionsLink;
+    this.warrantyLink = productDetails.warrantyLink;
+  }
+
+  extraInfoHTML() {
+    return `
+      <a href="${this.instructionsLink}" target ="_blank">
+      Instructions
+      </a>
+      <a href="${this.warrantyLink}" target ="_blank">
+      Warranty
+      </a>
+    `;
+  }
+}
+
 export const products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
@@ -129,7 +153,10 @@ export const products = [
       "toaster",
       "kitchen",
       "appliances"
-    ]
+    ],
+    type: "appliance",
+    instructionsLink: "images/appliance-instructions.png",
+    warrantyLink: "images/appliance-warranty.png"
   },
   {
     id: "3ebe75dc-64d2-4137-8860-1f5a963e534b",
@@ -314,7 +341,10 @@ export const products = [
       "water boiler",
       "appliances",
       "kitchen"
-    ]
+    ],
+    type: "appliance",
+    instructionsLink: "images/appliance-instructions.png",
+    warrantyLink: "images/appliance-warranty.png"
   },
   {
     id: "6b07d4e7-f540-454e-8a1e-363f25dbae7d",
@@ -619,7 +649,10 @@ export const products = [
       "coffeemakers",
       "kitchen",
       "appliances"
-    ]
+    ],
+    type: "appliance",
+    instructionsLink: "images/appliance-instructions.png",
+    warrantyLink: "images/appliance-warranty.png"
   },
   {
     id: "02e3a47e-dd68-467e-9f71-8bf6f723fdae",
@@ -679,7 +712,10 @@ export const products = [
       "food blenders",
       "kitchen",
       "appliances"
-    ]
+    ],
+    type: "appliance",
+    instructionsLink: "images/appliance-instructions.png",
+    warrantyLink: "images/appliance-warranty.png"
   },
   {
     id: "36c64692-677f-4f58-b5ec-0dc2cf109e27",
@@ -843,6 +879,9 @@ export const products = [
 ].map((productDetails) => {
   if (productDetails.type === 'clothing'){
     return new Clothing(productDetails);
+  }
+  else if (productDetails.type === 'appliance') {
+    return new Appliance(productDetails);
   }
   return new Product(productDetails);
 });
