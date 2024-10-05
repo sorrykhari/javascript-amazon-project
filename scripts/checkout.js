1971392b9036f0e8a9d51a5859e8f21004e4130e
Kai <kharimobley@gmail.com>
2024-10-05 01:08:19 -0400

    Add backend pracitce file

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,7 +1,7 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { renderCheckoutHeader } from "./checkout/checkoutHeader.js";
-import '../data/car.js';
+import "../data/backend-practice.js";
 
 renderCheckoutHeader(); 
 renderOrderSummary();
